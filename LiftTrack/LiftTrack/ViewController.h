b8473e25cffaf7246e1653225bf4dd5937caf966
Jerry Wong <jerry@Jerrys-iMac.local>
2015-05-01 00:41:10 -0400

    Base template.

---
+++ LiftTrack/LiftTrack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiftTrack
+//
+//  Created by Jerry Wong on 5/1/15.
+//  Copyright (c) 2015 Jerry Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
